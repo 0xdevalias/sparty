cce42a1e186c907dbe21afacb0dc491855042066
Glenn 'devalias' Grant <glenn@devalias.net>
2013-11-12 15:34:44 +1100

    Added an option to bypass the Python 2.6 version check

--- sparty_v_0.1.py
+++ sparty_v_0.1.py
@@ -93,7 +93,13 @@ def check_python():
         version = sys.version_info
         if version[:2] != (2,6):
                 print "[-] Sparty is written in Python 2.6.5 (final). Kindly use that version!"
-                sys.exit(0)
+                print "[devalias.net] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                print "[devalias.net] !!! HERE THERE BE DRAGONS !!!"
+                print "[devalias.net] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                unsupported_ver = raw_input("[devalias.net] Would you like to continue past this version check AT YOUR OWN RISK? (Type: YES): ")
+                if unsupported_ver != "YES":
+                    print "[devalias.net] Probably a safer choice. Exiting! (Entered: %s)" %(unsupported_ver)
+                    sys.exit(0)
 
 
 
